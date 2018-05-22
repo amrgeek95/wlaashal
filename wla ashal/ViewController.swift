550f4327a7dc66680c482c03b385ff9a49d69543
amr <amr.geek.95@gmail.com>
2018-05-22 04:27:36 +0200

    Initial Commit

---
+++ wla ashal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wla ashal
+//
+//  Created by amr sobhy on 5/22/18.
+//  Copyright © 2018 amr sobhy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
